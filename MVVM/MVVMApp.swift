075888b90cf6abfcc26da8cfdf5f9331ce2f8c1d
Haruki Hanagata <haruzo28@icloud.com>
2023-01-19 09:33:18 +0900

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by 花形春輝 on 2023/01/19.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
